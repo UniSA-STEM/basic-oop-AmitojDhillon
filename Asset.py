4b2b11d77bc530247bb73c19c89fb551761acd67
Amitoj Dhillon <Dhiay010@mymail.unisa.edu.au>
2025-10-13 15:29:38 +1030

    Update to Asset.py

--- Asset.py
+++ Asset.py
@@ -20,7 +20,7 @@ class Asset:
     def get_description(self):
         return self.__description
 
-    def is_encrypted(self):
+    def get_encrypted(self):
         return self.__encrypted
 
     def set_name(self, name):
@@ -31,3 +31,23 @@ class Asset:
 
     def set_encrypted(self, state):
         self.__encrypted = bool(state)
+
+    name = property(get_name, set_name)
+    description = property(get_description, set_description)
+    encrypted = property(get_encrypted, set_encrypted)
+
+
+    def encrypt(self):
+        self.__encrypted = True
+
+    def decrypt(self):
+        self.__encrypted = False
+
+
+    def __str__(self):
+
+        if self.__encrypted:
+            return f'{self.__name} {self.__description} [Encrypted]'
+
+        else:
+            return f'{self.__name} {self.__description}'
