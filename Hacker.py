624b1e90f687aa3e5a4ad21f00f28ec69edd4f87
Amitoj Dhillon <Dhiay010@mymail.unisa.edu.au>
2025-10-16 22:05:17 +1030

    Update to Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -6,6 +6,53 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
-class Hacker:
+from tkinter.font import names
 
-    def __init__(self):
+from Asset import Asset
+from Rig import Rig
+
+class Hacker:
+    TRACE_THRESHOLD = 5
+
+    def __init__(self, name):
+        self.__name = name
+        self.__inventory = [Asset("CryptoToken")]
+        self.__rig = None
+        self.__trace_level= 0
+        self.__exposed = False
+
+
+    def get_name(self):
+        return self.__name
+
+    def get_inventory(self):
+        return self.__inventory
+
+    def get_rig(self):
+        return self.__rig
+
+    def get_trace_level(self):
+        return self.__trace_level
+
+    def get_exposed(self):
+        return self.__exposed
+
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_rig(self, rig):
+        self.__rig = rig
+
+    def set_trace_level(self, value):
+        self.__trace_level = int(value)
+        self.__exposed = self.__trace_level >= Hacker.TRACE_THRESHOLD
+
+
+    name= property(get_name, set_name)
+    rig = property(get_rig, set_rig)
+    trace_level = property(get_trace_level, set_trace_level)
+    inventory = property(get_inventory)
+    exposed = property(get_exposed)
+
+
