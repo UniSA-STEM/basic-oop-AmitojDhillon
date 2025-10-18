223c61dc63fd71357eab0dac5ea365dd3d212dff
Amitoj Dhillon <Dhiay010@mymail.unisa.edu.au>
2025-10-18 16:15:45 +1030

    Update to Asset.py

--- Asset.py
+++ Asset.py
@@ -8,28 +8,65 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 """
 
 class Asset:
+    """
+    Attributes:
+        __name(str): The name of the asset.
+        __description(str): The description of the asset.
+        __encrypted(bool): Whether the asset is encrypted.
+    """
 
     def __init__(self, name, description, encrypted=False):
+        """
+        Constructor which initializes the asset.
+        :param name:The name of the asset.
+        :param description:The description of the asset.
+        :param encrypted:Whether the asset is encrypted.
+        """
         self.__name = name
         self.__description = description
         self.__encrypted = False
 
+    """
+    Getters
+    """
+
     def get_name(self):
+        """
+        returns the name of the asset.
+        """
         return self.__name
 
     def get_description(self):
+        """
+        returns the description of the asset.
+        """
         return self.__description
 
     def get_encrypted(self):
+        """
+        returns whether the asset is encrypted.
+        """
         return self.__encrypted
+    """
+    Setters
+    """
 
     def set_name(self, name):
+        """
+        Sets the name of the asset.
+        """
         self.__name = name
 
     def set_description(self, description):
+        """
+        Sets the description of the asset.\
+        """
         self.__description = description
 
     def set_encrypted(self, state):
+        """
+        Sets the encrypted state of the asset.
+        """
         self.__encrypted = bool(state)
 
     name = property(get_name, set_name)
@@ -38,13 +75,23 @@ class Asset:
 
 
     def encrypt(self):
+        """
+        Encrypts the asset.(sets encrypted state to True).
+        """
         self.__encrypted = True
 
     def decrypt(self):
+        """
+        Encrypts the asset.(sets encrypted state to False).
+        """
         self.__encrypted = False
 
 
     def __str__(self):
+        """
+        Returns the string representation of the asset with asset name and description.
+        If asset is encrypted, the string representation is will show [Encrypted] at the end.
+        """
 
         if self.__encrypted:
             return f'{self.__name} {self.__description} [Encrypted]'
